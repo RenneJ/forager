c5b9d44c51ae40f32624987b33f9fbeeb9844f69
Renne <renne.jamsen@gmail.com>
2024-12-01 15:30:40 +0200

    Added comments

--- utils/cloudstorage.js
+++ utils/cloudstorage.js
@@ -10,20 +10,21 @@ const now = new Date(Date.now());
 const nowFormat = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
 
 export const storeInCloud = async () => {
-		try {
-			const area = await fetchItem("area");
-			const basket = await parseStoredValue("basket");
-			if (basket && area) {
-				push(ref(database, "collection/" + auth.currentUser.uid), {
-					time: nowFormat,
-					area: area,
-					basket: basket
-				});
-				removeItems(["area", "basket"]);
-			} else {
-				console.log("Nothing to send.")
-			}
-		} catch (error) {
-			console.log("cs27", error)
+		const area = await fetchItem("area");
+		// string to JSON
+		const basket = await parseStoredValue("basket");
+		if (basket && area) {
+			push(ref(database, "collection/" + auth.currentUser.uid), {
+				time: nowFormat,
+				area: area,
+				basket: basket
+			});
+			removeItems(["area", "basket"]);
+		} else {
+			// Handle error in component
+			throw new Error("Cannot save empty basket.")
 		}
 }
+
+// wanted to implement fetch function but couldn't figure out
+// how to return value to component...
